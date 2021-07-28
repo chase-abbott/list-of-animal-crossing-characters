fc52a2656041dcbf188b73d64bb1191875d4b218
Chase <cabbott93@gmail.com>
2021-07-28 09:47:03 -0700

    completed detail page

--- src/components/VillagerDetail.jsx
+++ src/components/VillagerDetail.jsx
@@ -5,18 +5,44 @@ export default function VillagerDetail({ villager }) {
 
   return (
     <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center'
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr',
+      justifyContent: 'center',
+      backgroundColor: '#90ee90',
+      borderRadius: '25px',
+      cursor: 'pointer',
+      maxWidth: '66vw',
     }}>
-      <figure>
-        <img 
-          src={villager.image} 
-          alt={villager.id} 
-          style={{
-            width: '350px'
-          }}/>
+      <img 
+        src={villager.image} 
+        alt={villager.id} 
+        style={{
+          width: '350px',
+          maxHeight: '66vh',
+          display: 'flex',
+          justifySelf: 'flex-end'
+        }}/>
+      <figure style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+        <h1> 
+          {villager.name} 
+        </h1>
+        <p> {villager.japaneseName}</p>
+        <p> {villager.quote}</p>
+        <p> {villager.birthday}</p>
+        <ul>
+          Coffee Order
+          <li>{villager.coffee.roast}</li>
+          <li>{villager.coffee.milk}</li>
+          <li>{villager.coffee.sugar}</li>
+        </ul>
+        <a 
+          href={villager.song.link} 
+          target="_blank" 
+          rel="noreferrer"> Play My Song!</a>
       </figure>
     </div>
   );
